e20d34ae086a8c009b3097cabdb91e0ee6551e51
Matthew Fritsch <matthewmfritsch@gmail.com>
2024-07-03 01:15:24 -0700

    added a string function to TokenType; changed literal to be an interface{} to allow any value

--- token.go
+++ token.go
@@ -1,24 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 type Object interface {
 	toString() string
 }
 
 type Token struct {
-	ttype  TokenType
-	lexeme string
-	line   uint32
+	ttype   TokenType
+	lexeme  string
+	literal interface{}
+	line    uint32
 }
 
 func (token *Token) ToString() string {
-	return fmt.Sprintf("%s %s %s", string(token.ttype), token.lexeme)
+	literal_msg := "<no-literal>"
+	if stringval, ok := token.literal.(string); ok {
+		literal_msg = stringval
+	} else if floatval, ok := token.literal.(float64); ok {
+		literal_msg = strconv.FormatFloat(floatval, 'E', -1, 64)
+	}
+	return fmt.Sprintf("%v %s %s", token.ttype, string(token.lexeme), literal_msg)
 }
 
-func (token *Token) ToLiteral() interface{} {
-	if true {
-		return "nice"
-	}
-	return 1
-}
+// func (token *Token) ToLiteral() interface{} {
+// 	if token.ttype == NUMBER {
+// 		num, err := strconv.ParseInt(token.lexeme, 10, 64)
+// 		if err == nil {
+// 			error(0, fmt.Sprintf("Could not turn %s into integer", num))
+// 		}
+// 		return num
+// 	}
+// 	if token.ttype == STRING {
+// 		return token.lexeme
+// 	}
+// 	return nil
+// }

7c2f240c2a437a3dfbb44bdd4536315de355542d
Matthew Fritsch <matthewmfritsch@gmail.com>
2024-07-05 16:46:26 -0700

    basic expr def

---
+++ golox_app/expr.go
@@ -0,0 +1,19 @@
+package main
+
+type Expr interface {}
+
+type Binary struct {
+    left Expr
+    operator Token
+    right Expr
+}
+type Grouping struct {
+    expression Expr
+}
+type Literal struct {
+    value interface{}
+}
+type Unary struct {
+    operator Token
+    right Expr
+}
